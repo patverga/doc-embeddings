95c0eafd4ed388f2717e45ba2f20ad6d33a519db
pv <pat@cs.umass.edu>
2014-09-04 11:33:42 -0400

    working doc emnbed cluster and retuer reader

--- src/main/scala/co/pemma/MainClass.scala
+++ src/main/scala/co/pemma/MainClass.scala
@@ -1,16 +1,20 @@
 package main.scala.co.pemma
 
+import java.io.{BufferedInputStream, FileInputStream}
+import java.util
+import java.util.zip.GZIPInputStream
+
 import cc.factorie.app.nlp
-import cc.factorie.app.nlp.embeddings.{EmbeddingOpts, EmbeddingDistance, SkipGramNegSamplingEmbeddingModel}
+import cc.factorie.app.nlp.embeddings.{EmbeddingDistance, EmbeddingOpts, SkipGramNegSamplingEmbeddingModel}
 import cc.factorie.app.nlp.load.LoadPlainText
 import cc.factorie.app.nlp.{DocumentAnnotationPipeline, segment}
 import cc.factorie.la.DenseTensor1
-import org.apache.spark.{SparkConf, SparkContext}
+import weka.clusterers.SimpleKMeans
+import weka.core.{Attribute, DenseInstance, Instances}
 
-import org.apache.spark.mllib.clustering.KMeans
-import org.apache.spark.mllib.linalg.{Vectors}
-
+import scala.collection.JavaConversions._
 import scala.io.Source
+
 
 /**
  * Created by pv on 8/28/14.
@@ -21,51 +25,71 @@ object MainClass  extends  App{
   val corpus = "./data/corpus"
   val tokenizedDir = "./data/tokenized"
 
-//  tokenizeText()
-//  createEmbeddings()
-//  interactiveDistance()
+  val numClusters = 35
+  val numIterations = 250
+  
+  //  tokenizeText()
+  //  createEmbeddings()
+  //  interactiveDistance()
 
-  docStuff()
+  docStuff(readReuters("./2342newsML.xml.gz")._1)
+//  docStuff(readNaNewsData("./lw98.dat").head)
 
-  def docStuff()
+  def docStuff(doc :String)
   {
-    val numClusters = 5
-    val numIterations = 100
-
-    val docs = readNaNewsData("/home/pat/corpus/Na_news98/lw98.dat")
-    val doc = docs.head
-
     EmbeddingDistance.load("./vectors")
 
     // split the doc into words
-    val words = doc.split("\\s+").filter(w => !nlp.lexicon.StopWords.containsWord(w)  && w.size > 1 )
-    // turn each word into a spark vector
-    print("Converting fac vectors to spark vectors...")
-    val vectors = words.map(w => {
-      val facTensor = EmbeddingDistance.word2Vector(w)
-      if (facTensor != null) Vectors.dense(facTensor.toArray) else null
-    }).filter(_!=null)
+    val words = doc.split("\\s+").filter(w => !nlp.lexicon.StopWords.containsWord(w)  && w.size > 1)
+    // turn each word into an embedding vector
+    print("Converting fac vectors to weka instances...")
+    val wordTensors = words.map(w => {
+      EmbeddingDistance.word2Vector(w)
+    }).filter(_ != null)
 
-    val conf = new SparkConf().setAppName("pat").setMaster("local")
-    val sc = new SparkContext(conf)
-    val rdd = sc.parallelize(vectors)
+    clusterWords( doc, wordTensors)
+    sumWords(doc, wordTensors)
+
+    println(doc)
+  }
+
+  def sumWords(doc: String, wordTensors: Array[DenseTensor1])
+  {
+    val embedding_in = new DenseTensor1(wordTensors.head.size, 0)
+    wordTensors.foreach(word => embedding_in.+=(word))
+    embedding_in./=(wordTensors.size)
+    val words = EmbeddingDistance.nearestTensors(embedding_in, numClusters).map(_._1).mkString(", ")
+    println("SUM WORDS")
+    println(words)
+  }
+
+  def clusterWords(doc: String, wordTensors: Array[DenseTensor1])
+  {
+    val vecSize = wordTensors.head.size
+    val attributeList = new util.ArrayList[Attribute](vecSize + 1)
+    for (i <- 0 to vecSize - 1)
+      attributeList.add(new Attribute(i + ""))
+
+    val instances = new Instances("", attributeList, wordTensors.size)
+    wordTensors.foreach(t => {
+      instances.add(new DenseInstance(1, t.toArray))
+    })
     println(" Done.")
 
-    // run kmeans with the spark vectors
+    // run kmeans with the weka
     println("Running KMeans")
-    val km = new KMeans()
-    km.setK(numClusters)
-    km.setMaxIterations(numIterations)
-    val model = km.run(rdd)
+    val kmeans = new SimpleKMeans()
+    kmeans.setNumClusters(numClusters)
+    kmeans.setMaxIterations(numIterations)
+    kmeans.buildClusterer(instances)
 
-    model.clusterCenters.foreach(vector =>
-    {
-      val tensor = new DenseTensor1(vector.toArray)
-      val words = EmbeddingDistance.nearestNeighbors(tensor, 5)
-      words.foreach(w => println(w))
-      println()
-    })
-
+    val words = kmeans.getClusterCentroids.flatMap(centroid => {
+      val tensor = new DenseTensor1(centroid.toDoubleArray)
+      val centroidWords = EmbeddingDistance.nearestTensors(tensor, 1)
+      centroidWords.map(_._1)
+    }).mkString(", ")
+    println ("CLUSTER WORDS")
+    println(words)
   }
 
   def readNaNewsData(inputLoc : String) : Seq[String] =
@@ -84,6 +108,16 @@ object MainClass  extends  App{
     })
     println(" Done.")
     removeStart
+  }
+
+  def readReuters(inputLoc : String) : (String, Seq[String]) =
+  {
+    val fullDoc = Source.fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(inputLoc))))
+    val (text, codes) = fullDoc.getLines().filter(line => line.startsWith("<p>") || line.startsWith("  <code ")).toSeq.partition(_.startsWith("<p>"))
+    val textString = text.map(s => s.substring(3, s.length-4)).mkString("\n")
+    val codeStrings = codes.map(c => c.substring(c.indexOf("\"")+1, c.indexOf(">")-1))
+    print(codeStrings.mkString(", "))
+    (textString, codeStrings)
   }
 
   def tokenizeText()
@@ -114,7 +148,7 @@ object MainClass  extends  App{
 
 
   def createEmbeddings() {
-//    val inputs = new java.io.File(tokenizedDir).listFiles.toSeq.map(str => s" --train=$str")
+    //    val inputs = new java.io.File(tokenizedDir).listFiles.toSeq.map(str => s" --train=$str")
 
     val opts = new EmbeddingOpts()
     opts.parse(Seq(
@@ -136,35 +170,35 @@ object MainClass  extends  App{
     EmbeddingDistance.nearestNeighbours("vectors", 5)
   }
 
-//  class EmbeddingOpts extends CmdOptions {
-//
-//    // Algorithm related
-//    val dimension = new CmdOption("size", 200, "INT", "use <int> size of word vectors")
-//    val window = new CmdOption("window", 5, "INT", "use <int> skip length between words")
-//    val threads = new CmdOption("threads", 12, "INT", "use <int> threads")
-//    val negative = new CmdOption("negative", 1, "INT", "use <int> number of negative examples")
-//    val minCount = new CmdOption("min-count", 5, "INT", "This will discard words that appear less than <int> times; default is 5")
-//    val ignoreStopWords = new CmdOption("ignore-stopwords", false, "BOOLEAN", "use <bool> to include or discard stopwords. Use 1 for discarding stopwords")
-//    val cbow = new CmdOption("cbow", false, "BOOLEAN", "user cbow=true for cbow and cbow=false for skip-gram") // 1 would be SkipGram // default method is skipgram
-//    val sample = new CmdOption("sample", 0.001, "DOUBLE", "use <double> subsampling")
-//
-//    // Optimization related (Don't change if you do not understand how vectors are initialized)
-//    val rate = new CmdOption("rate", 0.025, "DOUBLE", "learning rate for adaGrad")
-//    val delta = new CmdOption("delta", 0.1, "DOUBLE", "delta for adaGrad")
-//
-//    // IO Related (MUST GIVE Options)
-//    val encoding = new CmdOption("encoding", "UTF8", "STRING", "use <string> for encoding option. ISO-8859-15 is default")
-//    val saveVocabFile = new CmdOption("save-vocab", "", "STRING", "save vocab file")
-//    val loadVocabFile = new CmdOption("load-vocab", "", "STRING", "load the vocab file") // atleast one of them  should be given. save-vocab or load-vocab
-//    val corpus = new CmdOption("train", "", "STRING", "train file")
-//    val output = new CmdOption("output", "", "STRING", "Use <file> to save the resulting word vectors")
-//    val binary = new CmdOption("binary", false, "BOOLEAN", "use true for storing .gz format and false for plain txt format. Both stores in ISO-8859-15 Encoding")
-//
-//    // Vocabulary related
-//    // Maximum 14.3M * 0.7 = 10M words in the vocabulary (Don;t change if you understand how vocabBuilder works)
-//    val vocabSize = new CmdOption("max-vocab-size", 2e6.toInt, "INT", "Max Vocabulary Size. Default Value is 2M . Reduce to 200k or 500k is you learn embeddings on small-data-set")
-//    val vocabHashSize = new CmdOption("vocab-hash-size", 14.3e6.toInt, "INT", "Vocabulary hash size")
-//    val samplingTableSize = new CmdOption("sampling-table-size", 1e8.toInt, "INT", "Sampling Table size")
-//  }
+  //  class EmbeddingOpts extends CmdOptions {
+  //
+  //    // Algorithm related
+  //    val dimension = new CmdOption("size", 200, "INT", "use <int> size of word vectors")
+  //    val window = new CmdOption("window", 5, "INT", "use <int> skip length between words")
+  //    val threads = new CmdOption("threads", 12, "INT", "use <int> threads")
+  //    val negative = new CmdOption("negative", 1, "INT", "use <int> number of negative examples")
+  //    val minCount = new CmdOption("min-count", 5, "INT", "This will discard words that appear less than <int> times; default is 5")
+  //    val ignoreStopWords = new CmdOption("ignore-stopwords", false, "BOOLEAN", "use <bool> to include or discard stopwords. Use 1 for discarding stopwords")
+  //    val cbow = new CmdOption("cbow", false, "BOOLEAN", "user cbow=true for cbow and cbow=false for skip-gram") // 1 would be SkipGram // default method is skipgram
+  //    val sample = new CmdOption("sample", 0.001, "DOUBLE", "use <double> subsampling")
+  //
+  //    // Optimization related (Don't change if you do not understand how vectors are initialized)
+  //    val rate = new CmdOption("rate", 0.025, "DOUBLE", "learning rate for adaGrad")
+  //    val delta = new CmdOption("delta", 0.1, "DOUBLE", "delta for adaGrad")
+  //
+  //    // IO Related (MUST GIVE Options)
+  //    val encoding = new CmdOption("encoding", "UTF8", "STRING", "use <string> for encoding option. ISO-8859-15 is default")
+  //    val saveVocabFile = new CmdOption("save-vocab", "", "STRING", "save vocab file")
+  //    val loadVocabFile = new CmdOption("load-vocab", "", "STRING", "load the vocab file") // atleast one of them  should be given. save-vocab or load-vocab
+  //    val corpus = new CmdOption("train", "", "STRING", "train file")
+  //    val output = new CmdOption("output", "", "STRING", "Use <file> to save the resulting word vectors")
+  //    val binary = new CmdOption("binary", false, "BOOLEAN", "use true for storing .gz format and false for plain txt format. Both stores in ISO-8859-15 Encoding")
+  //
+  //    // Vocabulary related
+  //    // Maximum 14.3M * 0.7 = 10M words in the vocabulary (Don;t change if you understand how vocabBuilder works)
+  //    val vocabSize = new CmdOption("max-vocab-size", 2e6.toInt, "INT", "Max Vocabulary Size. Default Value is 2M . Reduce to 200k or 500k is you learn embeddings on small-data-set")
+  //    val vocabHashSize = new CmdOption("vocab-hash-size", 14.3e6.toInt, "INT", "Vocabulary hash size")
+  //    val samplingTableSize = new CmdOption("sampling-table-size", 1e8.toInt, "INT", "Sampling Table size")
+  //  }
 
 }
