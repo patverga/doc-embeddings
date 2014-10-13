d892f4ee32afc3e51906e0524add2ad903d55ccf
pv <pat@cs.umass.edu>
2014-10-13 13:54:48 -0400

    use book maps to export data

--- src/main/scala/co/pemma/books/BookNer.scala
+++ src/main/scala/co/pemma/books/BookNer.scala
@@ -1,6 +1,6 @@
 package co.pemma.books
 
-import java.io.{PrintWriter, BufferedWriter, FileWriter}
+import java.io.{File, PrintWriter, BufferedWriter, FileWriter}
 
 /**
  * Created by pat on 10/9/14.
@@ -8,15 +8,40 @@ import java.io.{PrintWriter, BufferedWriter, FileWriter}
 object BookNer extends App
 {
 
-  val file = "/home/pat/doc-embeddings/books/NER/0529370.0036.011.umich.edu.out"
-  loadBookFromOwpl(file, "test")
+  val bookYear = loadMap("books/book-year-key")
+  val bookDir = loadMap("books/book-ner-dir-key")
+
+  bookYear.zipWithIndex.foreach{case((book, year),i) =>
+    val inFile = bookDir.getOrElse(book, null)
+    if (inFile != null){
+      val outFile = s"books/$year/$book"
+      println(inFile, outFile)
+      loadBookFromOwpl(inFile, outFile)
+    }
+  }
+
+  def loadMap(file : String): Map[String, String] =
+  {
+    val source = scala.io.Source.fromFile(file)
+    val map = source.getLines().map(line => {
+      val parts = line.split("\\s+")
+      if (parts.length == 2) parts(0) -> parts(1)
+      else null
+    }).filterNot(_ == null).toMap
+    source.close()
+    map
+  }
+
 
   def loadBookFromOwpl(inFile : String, outFile:String): Unit =
   {
-    val source = scala.io.Source.fromFile(file)
+    val source = scala.io.Source.fromFile(inFile)
     val lines = source.getLines()
 
-    val writer = new PrintWriter(new BufferedWriter(new FileWriter(outFile, true)))
+    // export the books into date directories
+    val file = new File(outFile)
+    file.mkdirs()
+    val writer = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))
     var sentence = new StringBuilder
     while (lines.hasNext)
     {
