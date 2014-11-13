bae5ee60c22e22f6529652716cabd2f30c149fe3
pv <pat@cs.umass.edu>
2014-11-13 15:46:31 -0500

    plot subject year histogram

--- books/subject-year-histogram.py
+++ books/subject-year-histogram.py
@@ -1,8 +1,12 @@
 import gzip
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
-min_year = 165
+min_year = 180
 max_year = 201
+min_time_slices = 17
+plot_max = 10000
+
 subject_years_counts = defaultdict(lambda: [0] * (max_year-min_year))
 
 f = gzip.open('meta-data2.gz')
@@ -15,10 +19,19 @@ for line in f:
             index = (date-min_year)
             subject_years_counts[subject][index] += 1
 
+fig = plt.figure()
+ax = plt.subplot(111)
 for subject, years in subject_years_counts.iteritems():
     non_zeros = [year for year in years if year != 0]
-    if len(non_zeros) > 25:
+    if len(non_zeros) > min_time_slices:
         print (subject, years)
+        ax.plot(range(min_year*10, max_year*10, 10),
+                [min(plot_max, year) for year in years], marker='x', label=subject)
+
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.7, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5),prop={'size':10})
+plt.show()
 
 
 
