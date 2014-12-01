138b4ad6177b5a177b233ef77eb7c1b94de8e470
pv <pat@cs.umass.edu>
2014-12-01 16:06:27 -0500

    use subject map for book queries

--- books/mapable_subjects.py
+++ books/mapable_subjects.py
@@ -1,8 +1,11 @@
 __author__ = 'pv'
 
 import gzip
+import sys
 from collections import defaultdict
 
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def map_subjects():
     book_file = gzip.open('meta-data2.gz', 'r')
@@ -27,16 +30,25 @@ def map_subjects():
 
     return subjects_mapped
 
+
 def map_subjects_IDS():
     subject_id_map = defaultdict()
     # get the subjects we can map
     with open("classifications/all-unfiltered") as f:
         for line in f.readlines():
             parts = line.decode('utf-8').replace(u'\u2011', "-").split("\t", 1)
-            ID = parts[0].strip()
-            if len(parts) > 1 and len(ID) > 0:
-                subject_id_map[parts[1].strip()] = ID
+            id = parts[0].strip()
+            if len(parts) > 1 and len(id) > 0:
+                subject_id_map[parts[1].strip()] = id
 
     return subject_id_map
 
-map_subjects_IDS()
+
+def export(dictionary):
+    with open("/home/pv/doc-embeddings/src/main/resources/subject-id-map", "w") as w:
+        for key, val in dictionary.iteritems():
+            if key:
+                w.write(key+'|'+val+"\n")
+
+
+export(map_subjects_IDS())
--- src/main/scala/co/pemma/books/BookQueries.scala
+++ src/main/scala/co/pemma/books/BookQueries.scala
@@ -16,29 +16,35 @@ object BookQueries
   def main(args: Array[String])
   {
     // set some params
-    val numDocs = 10000
+    val numDocs = 1000
     val output = "./books/output/"
 
     assert(args.size > 0, " Must supply a query id number.")
     val qid = Integer.parseInt(args(0))
 
     // read in queries
-    val source = Source.fromURL(getClass.getResource("/book_queries"))
+    val querySource = Source.fromURL(getClass.getResource("/book_queries"))
 //    val source = Source.fromFile("./book_queries")
-    val queries = source.getLines().toList
-    source.close()
+    val queries = querySource.getLines().toList
+    querySource.close()
     val query = queries(qid)
+
+    // read in subjectmap
+    val subjectSource = Source.fromURL(getClass.getResource("/subject-id-map"))
+    //    val source = Source.fromFile("./book_queries")
+    val subjects = subjectSource.getLines().map(line => { val parts = line.split("\\|"); parts(0) -> parts(1) }).toMap
+    subjectSource.close()
+
+    // make sure this subject is mappable
+    assert(subjects.contains(query))
 
     // initialize things
 //      val bookIndex = List("./index/books-index_small", "./index/wikipedia").asJava
-//    val bookIndex = "./index/pages-index_20"
-    val bookIndex = (for (i <- 0 to 20; if i != 14; num = if (i < 10) s"0$i"; else s"$i")
-    yield s"/work2/manmatha/michaelz/galago/Proteus/Proteus/homer/mzShards/pages-index_$num").toList.asJava
-
-    // debug stupid galago swarm index
-//    val bookIndex = (for (i <- 1 to args.size-1;in = Integer.parseInt(args(i)); num = if (in < 10) s"0$in"; else s"$in")
+    val bookIndex = "./index/pages-index_20"
+//    val bookIndex = (for (i <- 0 to 20; if i != 14; num = if (i < 10) s"0$i"; else s"$i")
 //    yield s"/work2/manmatha/michaelz/galago/Proteus/Proteus/homer/mzShards/pages-index_$num").toList.asJava
-    println(bookIndex.toString)
+
+   println(bookIndex.toString)
 
     val indexParam = new Parameters()
     indexParam.set("index", bookIndex)
@@ -55,18 +61,22 @@ object BookQueries
 
     // write results to file
     val langRegex = "[eE]ng(?:lish)?".r
-    val p = new java.io.PrintWriter(s"$output/$qid")
+    val p = new java.io.PrintWriter(s"$output/$qid-${subjects(query)}")
     try {
       collectionDocs.foreach { case (rank, score, doc) =>
         val lang = doc.metadata.get("language")
         val subject = doc.metadata.get("subject")
         val year = doc.metadata.get("year")
-        if (year != null && subject != null  && lang != null && langRegex.pattern.matcher(lang).matches())
-          p.write(s"$qid \t $rank \t $score \t ${doc.name} \t $subject \t $year \t $lang \n")
+        if (year != null && subject != null  && lang != null && subjects.contains(subject) && langRegex.pattern.matcher(lang).matches())
+          p.write(s"$qid \t $rank \t $score \t ${doc.name} \t $subject \t ${subjects(subject)} \t $year \n")
       }
     }
     finally {
       p.close()
     }
+  }
+
+  def loadMap(): Unit ={
+
   }
 }
