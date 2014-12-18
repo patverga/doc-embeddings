514192927a6087ab8494798a2581b584ffa3c536
pv <pat@cs.umass.edu>
2014-12-18 17:21:41 -0500

    time drift rm calculator

--- src/main/scala/co/pemma/books/TimeDriftTest.scala
+++ src/main/scala/co/pemma/books/TimeDriftTest.scala
@@ -1,8 +1,10 @@
 package co.pemma.books
 
+import java.io.File
+
 import co.pemma.ExpansionModels
-import co.pemma.embeddings.{WordVectorsSerialManager, WordVectorMath}
-import edu.umass.ciir.strepsimur.galago.{GalagoQueryLib, GalagoQueryBuilder, GalagoSearcher}
+import co.pemma.embeddings.{WordVectorMath, WordVectorsSerialManager}
+import edu.umass.ciir.strepsimur.galago.{GalagoQueryBuilder, GalagoSearcher}
 import org.apache.commons.lang3.StringUtils
 
 /**
@@ -10,21 +12,49 @@ import org.apache.commons.lang3.StringUtils
  */
 object TimeDriftTest extends BookTimeSearcher
 {
+  val expTerms = 10
+
   def main(args: Array[String]) {
     val (qid: Int, query: String, subjects: Map[String, String], searcher: GalagoSearcher, cleanQuery: String) = initialize(args)
     val sdmQuery = GalagoQueryBuilder.seqdep(cleanQuery).queryStr
 
-    val wordVecs = new WordVectorMath(WordVectorsSerialManager.deserializeWordVectors("./vectors/decade-vectors/180-194.vectors.dat"))
-    val wordVecExpansionTerms = wordVecs.stringNearestNeighbors(cleanQuery)
-    wordVecExpansionTerms.foreach(println(_))
+//    val minWordVecs = new WordVectorMath(WordVectorsSerialManager.deserializeWordVectors(s"./vectors/decade-vectors/$minDate.vectors.dat"))
+//    val wordVecExpansionTerms = minWordVecs.nearestNeighbors(minWordVecs.word2Vec()cleanQuery, usePhrases = false)
+//    wordVecExpansionTerms.foreach(println(_))
 
-    println("Running timeslice queries...")
+    // querying each decade seperately
     val decadeTerms = for (decade <- maxDate to minDate by -10)
-//      sdmRankings = ExpansionModels.runDecadeQuery(maxDate, sdmQuery, "robust", searcher, numResultDocs);
-//      decadeExpansionTerms = ExpansionModels.lce(sdmRankings take numExpansionDocs, searcher, numExpansionTerms).
-//        filterNot(term => yearRegex.pattern.matcher(term._1).matches());
-     yield  ExpansionModels.lce(ExpansionModels.runDecadeQuery(maxDate, sdmQuery, "robust", searcher, numResultDocs)
-        take numExpansionDocs, searcher, numExpansionTerms).filterNot(term => yearRegex.pattern.matcher(term._1).matches());
+     yield decade ->ExpansionModels.lce(ExpansionModels.runDecadeQuery(decade, sdmQuery, "robust", searcher, numResultDocs)
+        take numExpansionDocs, searcher, expTerms).filterNot(term => yearRegex.pattern.matcher(term._1).matches())
+
+    // querying all data at once
+    val sdmRankings = searcher.retrieveScoredDocuments(sdmQuery, None, numResultDocs)
+    val allTerms = (decadeTerms :+ (0 -> ExpansionModels.lce(sdmRankings take numExpansionDocs, searcher, expTerms).
+      filterNot(term => yearRegex.pattern.matcher(term._1).matches()))).toMap
+
+    export(decadeTerms.toMap, query, qid)
+  }
+
+  def export(termMap : Map[Int, Seq[(String, Double)]], query : String, qid : Int): Unit =
+  {
+    new File("./books/output/query-drift/").mkdirs()
+    val printer = new java.io.PrintWriter(s"./books/output/query-drift/$qid")
+
+    val uniqueTerms = termMap.flatMap(_._2).flatMap(_._1).toSet.size
+    val spanTerms = (termMap(minDate)++termMap(maxDate)).flatMap(_._1).toSet.size
+
+    println(uniqueTerms, spanTerms)
+    try {
+      termMap.toSeq.sortBy(_._1).foreach{case (decade, terms) =>
+        printer.write(decade +"\t")
+        terms.foreach(t => printer.write(t._1 + "\t"))
+        printer.write("\n")
+      }
+      printer.write(query + "\t" + uniqueTerms + "\t" + spanTerms)
+    }
+    finally {
+      printer.close()
+    }
   }
 }
 
