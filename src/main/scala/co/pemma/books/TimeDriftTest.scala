d113966fec01d7beeb5fb7340fef25917744fd76
pv <pat@cs.umass.edu>
2014-12-30 13:32:23 -0500

    filter unretrievable queries

--- src/main/scala/co/pemma/books/TimeDriftTest.scala
+++ src/main/scala/co/pemma/books/TimeDriftTest.scala
@@ -3,7 +3,6 @@ package co.pemma.books
 import java.io.File
 
 import co.pemma.ExpansionModels
-import co.pemma.embeddings.{WordVectorUtils, WordVectorsSerialManager}
 import edu.umass.ciir.strepsimur.galago.{GalagoQueryBuilder, GalagoSearcher}
 import org.apache.commons.lang3.StringUtils
 
